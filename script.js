3cf71e1617396e88f4575d08f1091e30f6d5db99
Saskia vd Kooij <84019788+saskiavanderkooij@users.noreply.github.com>
2021-06-12 16:28:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const pizzaBakken = function () {
+    console.log("Haal de pizza uit de vriezer.");
+    console.log("Zet de oven aan op de aanbevolen temperatuur.");
+    console.log("Bak de pizza in de oven voor de aangegeven tijd.");
+    console.log("Wanneer de timer afgaat haal je de pizza uit de oven.");
+    console.log("Eet de pizza. PAS OP IS HEET!");
+};
+
+const sushiMaken = function () {
+    console.log("Kook sushi rijst.");
+    console.log("Verdeel de rijst over het nori vel.");
+    console.log("Leg de zalm plakjes op de rijst.");
+    console.log("Rol het geheel op.");
+    console.log("Snij de rol in gelijke delen");
+    console.log("Eet de sushi");
+}
+
+
+sushiMaken();
+sushiMaken();
+sushiMaken();
+pizzaBakken();
+pizzaBakken();
